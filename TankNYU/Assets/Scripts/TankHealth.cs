8d28ef3f2b4935fc2885762a16f53a2b72f444fd
slvr7 <wbwb23@yeah.net>
2019-03-12 14:13:31 -0400

    Add files via upload

---
+++ TankNYU/Assets/Scripts/TankHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour
+{
+    public float TankMaxHealth;
+    private float TankCurentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TankCurentHealth = TankMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamaged(float damage)
+    {
+        TankCurentHealth -= damage;
+    }
+
+    public float getCurrentHealth()
+    {
+        return TankCurentHealth;
+    }
+}
